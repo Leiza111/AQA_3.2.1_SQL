15a81d8bbdd131b6b181f1f6b75d19ecb894923d
Leiza Girfatova <leiza-rad@yandex.ru>
2022-03-22 14:09:56 +0300

    first commit

---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,53 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Configuration;
+
+import lombok.val;
+
+import org.junit.jupiter.api.AfterAll;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest {
+
+    @AfterAll
+    static void deletingDataFromTheDb() {
+        DataHelper.DeleteInfo.deletingData();
+    }
+
+    @Test
+    public void shouldAuthorizationIsSuccessful() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999"); //Открыть приложение
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.VerificationCode.getAuthCode(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    public void shouldGiveErrorIfPasswordInvalid() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999"); //Открыть приложение
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getInvalidAuthInfo();
+        loginPage.invalidLogin(authInfo);
+        loginPage.getNotificationAuth();
+    }
+
+    @Test
+    public void shouldGiveLockIfAnInvalidPasswordIsEnteredThreeTimes() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999"); //Открыть приложение
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getInvalidAuthInfo();
+        loginPage.invalidPasswordThreeTimes(authInfo);
+        loginPage.getNotification();
+    }
+}
